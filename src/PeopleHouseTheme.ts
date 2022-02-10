534f1a0986a96889c63e64a4dd0520efcc1db223
Martin Skovsen <martin@billundskovsen.dk>
2022-02-10 14:16:17 +0100

    Create PeopleHouseTheme.ts

---
+++ src/PeopleHouseTheme.ts
@@ -0,0 +1,41 @@
+﻿import { createMuiTheme } from '@material-ui/core';
+
+const belowThisBreakpointIsMobile: number | 'xs' | 'sm' | 'md' | 'lg' | 'xl' =
+  'sm';
+
+export function getBreakpointForMobile() {
+  return belowThisBreakpointIsMobile;
+}
+
+const legoGrey = '#565F67';
+const legoBlack = '#202124';
+const legoGreen = '#1D5A52';
+const legoLightGreen = '#C7D599';
+const legoWhite = '#FFFFFF';
+const legoYellow = '#FAED00';
+
+const peopleHouseTheme = createMuiTheme({
+  palette: {
+    text: {
+      primary: legoBlack,
+      secondary: legoGrey,
+    },
+    background: {
+      default: legoYellow,
+      paper: legoWhite,
+    },
+    primary: {
+      light: legoLightGreen,
+      main: legoGreen,
+    },
+    secondary: {
+      main: legoYellow,
+    },
+  },
+  typography: {
+    fontFamily: ['"Open Sans"'].join(','),
+  },
+  overrides: {},
+});
+
+export default peopleHouseTheme;
