657ad3651e933de074fbfc3e28dd007c281b3b7c
Martin Skovsen <martin@billundskovsen.dk>
2022-02-10 19:55:32 +0100

    Add state handling in facility data page

--- src/pages/facility-data/FacilityData.tsx
+++ src/pages/facility-data/FacilityData.tsx
@@ -1,5 +1,26 @@
+import { Grid } from '@material-ui/core';
+import { BarChart } from '@material-ui/icons';
+import { useEffect, useState } from 'react';
+
 function FacilityData() {
-  return <div />;
+  const [isLoading, setIsLoading] = useState(true);
+  const [initialLoadHasCompleted, setInitialLoadHasCompleted] = useState(false);
+  const [facilityData, setFacilityData] = useState([]);
+
+  useEffect(() => {
+    // call
+    setIsLoading(true);
+    setFacilityData([]);
+    setIsLoading(false);
+    setInitialLoadHasCompleted(true);
+  }, []);
+
+  useEffect(() => {
+    if (initialLoadHasCompleted)
+      console.log('Do dependency related operations');
+  }, [facilityData]);
+
+  return <Grid container>{isLoading && <BarChart />}</Grid>;
 }
 
 export default FacilityData;
