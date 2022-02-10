deafd6e76466be3dda32db543a44c0c5331a1a52
Martin Skovsen <martin@billundskovsen.dk>
2022-02-10 15:06:21 +0100

    Create useIsOnSmallScreen.ts

---
+++ src/common/hooks/useIsOnSmallScreen.ts
@@ -0,0 +1,11 @@
+﻿import { useMediaQuery, useTheme } from '@material-ui/core';
+import { getBreakpointForMobile } from '../../PeopleHouseTheme';
+
+function useIsOnSmallScreen() {
+  const theme = useTheme();
+  return useMediaQuery(theme.breakpoints.down(getBreakpointForMobile()), {
+    noSsr: true,
+  });
+}
+
+export default useIsOnSmallScreen;
