224f4e0c7b88a5e15223e3080221a8c1df7eee35
Martin Skovsen <30232520+MartinSkovsen@users.noreply.github.com>
2022-02-11 09:45:18 +0100

    Create Events.tsx

---
+++ src/pages/events/Events.tsx
@@ -0,0 +1,22 @@
+import { Grid } from '@material-ui/core';
+
+function Events() {
+  return (
+    <Grid container>
+      <Grid item sm={3}>
+        Testing
+      </Grid>
+      <Grid item sm={3}>
+        Testing
+      </Grid>
+      <Grid item sm={3}>
+        Testing
+      </Grid>
+      <Grid item sm={3}>
+        Testing
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Events;
