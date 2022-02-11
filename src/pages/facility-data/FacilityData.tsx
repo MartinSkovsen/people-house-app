2152c17b32b1632317b100cf2266795fc252d198
Martin Skovsen <30232520+MartinSkovsen@users.noreply.github.com>
2022-02-11 09:01:27 +0100

    Add JSX content to facility data page

--- src/pages/facility-data/FacilityData.tsx
+++ src/pages/facility-data/FacilityData.tsx
@@ -1,26 +1,60 @@
-import { Grid } from '@material-ui/core';
-import { BarChart } from '@material-ui/icons';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
 import { useEffect, useState } from 'react';
+import { FacilityDataResponse } from '../../core/facility-data/FacilityData.types';
+import { getFacilityData } from '../../core/facility-data/FacilityDataService';
 
 function FacilityData() {
   const [isLoading, setIsLoading] = useState(true);
-  const [initialLoadHasCompleted, setInitialLoadHasCompleted] = useState(false);
-  const [facilityData, setFacilityData] = useState([]);
+  const [facilityData, setFacilityData] = useState<FacilityDataResponse>({
+    dateInterval: 'Day',
+    data: [],
+  });
 
   useEffect(() => {
-    // call
     setIsLoading(true);
-    setFacilityData([]);
-    setIsLoading(false);
-    setInitialLoadHasCompleted(true);
+    getFacilityData()
+      .then((data) => {
+        setFacilityData(data);
+        setIsLoading(false);
+      })
+      .catch(() => setIsLoading(false)); // Set bad state as well here
   }, []);
 
-  useEffect(() => {
-    if (initialLoadHasCompleted)
-      console.log('Do dependency related operations');
-  }, [facilityData]);
-
-  return <Grid container>{isLoading && <BarChart />}</Grid>;
+  return isLoading ? (
+    <Skeleton />
+  ) : (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Facility</TableCell>
+            <TableCell align="right">Consumption</TableCell>
+            <TableCell align="right">{facilityData.dateInterval}</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {facilityData.data.map((row) => (
+            <TableRow key={row.facility.address}>
+              <TableCell component="th" scope="row">
+                {row.facility.address}
+              </TableCell>
+              <TableCell align="right">{row.consumption}</TableCell>
+              <TableCell align="right">{row.dateLabel}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
 export default FacilityData;
